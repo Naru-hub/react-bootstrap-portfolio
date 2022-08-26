8ed806999656ea0074ee4962d6c4ead196b5ed81
Naru-hub <1208ny0324@gmail.com>
2022-08-27 00:35:20 +0900

    BrowserRouterでルーティング設定

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import "./App.css";
 import BlogPage from "./components/BlogPage";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <Router>
+      <div className="App">
+        <Header />
 
-      <HomePage />
-      <BlogPage />
-    </div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blog" element={<BlogPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
